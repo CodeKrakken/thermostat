eb16831e22e6001ea8a5c893218bb5441e82d1ff
Donald Newholm <newholm1@hotmail.com>
2019-11-15 15:52:13 +0000

    Added sound to buttons. Wicked.

--- jasmine-standalone-3.5.0/src/interface.js
+++ jasmine-standalone-3.5.0/src/interface.js
@@ -4,29 +4,43 @@ $(document).ready(function() {
     function updateTemperature() {
         $('#temperature').text(thermostat.getTemp());
         $('#temperature').attr('class', thermostat.usageMode());
-    } 
+    }
+
+    function basicScratch() {
+        $('audio#basicScratch')[0].play();
+    }
+
+    function resetScratch() {
+        $('audio#resetScratch')[0].play();
+    }
 
     updateTemperature();
     $('#temperature-up').on('click', function() {
+        basicScratch();
         thermostat.temperatureRaise(1);
         updateTemperature();
     })
     $('#temperature-down').on('click', function() {
+        basicScratch();
         thermostat.temperatureLower(1);
         updateTemperature();
     })
     $('#temperature-reset').on('click', function() {
+        resetScratch();
         thermostat.reset();
         updateTemperature();
     })
     $('#powersaveon').on('click', function() {
+        basicScratch();
         thermostat.PSMOn();
         $('#power-saving-status').text('on!!!')
         updateTemperature();
     })
     $('#powersaveoff').on('click', function() {
+        basicScratch();
         thermostat.PSMOff();
         $('#power-saving-status').text('off!!!')
         updateTemperature();
     })
+
 })
